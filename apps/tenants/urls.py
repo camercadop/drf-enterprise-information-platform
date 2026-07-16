bb352d1bc52ab9593e28fc5de082d0d44378bd25
Carlos Mercado <carlosmercadop714@gmail.com>
2026-07-15 23:50:15 -0500

    refactor (tenant): reorder router registrations

--- apps/tenants/urls.py
+++ apps/tenants/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "tenants"
 
 router = DefaultRouter()
-router.register("", views.TenantViewSet, basename="tenant")
 router.register("memberships", views.MembershipViewSet, basename="membership")
+router.register("", views.TenantViewSet, basename="tenant")
 
 urlpatterns = router.urls
